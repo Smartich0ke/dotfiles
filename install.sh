ae95e617bca6c8775f290a98ad936cc18f9fddce
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-03-07 07:08:50 +1030

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+rm ~/.profile
+rm ~/.gitconfig
+rm ~/.bash_profile
+rm ~/.bashrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.profile ~/.profile
@@ -9,11 +13,11 @@ echo 'Symlinks created'
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-echo 'Installing all packages expressly installed by apt (packages-apt-development.txt)... This may take a while...'
-sudo apt-get -y --ignore-missing install $(cat packages-apt-development.txt)
+echo 'Installing all packages expressly installed by apt (packages/packages-apt-development.txt)... This may take a while...'
+sudo apt-get -y --ignore-missing install $(cat packages/packages-apt-development.txt)
 
-echo 'installing all packages expressly installed by pip (packages-pip.txt)... This may take a while...'
-sudo pip install $(cat packages-pip.txt)
+echo 'installing all packages expressly installed by pip (packages/packages-pip.txt)... This may take a while...'
+sudo pip install $(cat packages/packages-apt-development.txt)
 
 echo 'installing nvm...'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
