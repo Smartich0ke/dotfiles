e9ef9b20995cde6ff43549f064aa45159fbb30a4
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-12-30 21:58:35 +1030

    commit the dos

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+
+alias python=python3
+alias k=kubectl
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+end
+
+export PICO_SDK_PATH=/home/npatrick/Code/pico/pico-sdk
+export PICO_EXAMPLES_PATH=/home/npatrick/Code/pico/pico-examples
+export PICO_EXTRAS_PATH=/home/npatrick/Code/pico/pico-extras
+export PICO_PLAYGROUND_PATH=/home/npatrick/Code/pico/pico-playground
+
+set -gx KUBECONFIG ~/.kube/pleiades.yaml
+
+export NVM_SYMLINK_CURRENT=true
+set -gx NVM_SYMLINK_CURRENT true
+
+starship init fish | source
