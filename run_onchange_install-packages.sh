63eaf8d8dc42dc1858be2e0eb335705410d6f679
Smartich0ke <41409702+Smartich0ke@users.noreply.github.com>
2023-12-31 12:12:07 +1030

    add colours and fix nerdfont installation

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/bash
+ 
+CYAN="36"
+GREEN="32"
+BOLDGREEN="\e[1;${GREEN}m"
+PLAINGREEN="\e[${GREEN}m"
+BOLDCYAN="\e[1;${CYAN}m"
+PLAINCYAN="\e[${CYAN}m"
+ENDCOLOUR="\e[0m"
 
-echo "Start package instatllation script"
+echo -e "${BOLDGREEN}Start package instatllation script. ${ENDCOLOUR}"
 
-echo "Installing packages..."
+echo -e "${PLAINGREEN}Installing packages...${ENDCOLOUR}"
 
 sudo apt update
 
@@ -26,56 +34,65 @@ sudo nala install -y \
     gnome-software-plugin-flatpak \
     aptitude \
 
-echo "Add flathub..."
+echo -e "${PLAINGREEN}Add flathub...${ENDCOLOUR}"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-echo "Installing fonts..."
+echo -e "${PLAINGREEN}Installing fonts...${ENDCOLOUR}"
 
 sudo nala install fonts-firacode
-git clone https://github.com/ryanoasis/nerd-fonts.git
-./install.sh FiraCode
+mkdir -p ~/.local/share/fonts
+FONT_DIR="$HOME/.local/share/fonts"
+curl -fLo "$FONT_DIR/FiraCodeNerdFontMono-Regular.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf
+curl -fLo "$FONT_DIR/FiraCodeNerdFontMono-Medium.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Medium/FiraCodeNerdFontMono-Medium.ttf
+curl -fLo "$FONT_DIR/FiraCodeNerdFontMono-Bold.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Bold/FiraCodeNerdFontMono-Bold.ttf
+curl -fLo "$FONT_DIR/FiraCodeNerdFontMono-Light.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Light/FiraCodeNerdFontMono-Light.ttf
+curl -fLo "$FONT_DIR/FiraCodeNerdFontMono-SemiBold.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/SemiBold/FiraCodeNerdFontMono-SemiBold.ttf
 
-echo "Installing Fish..."
+
+echo -e "${PLAINGREEN}Installing Fish...${ENDCOLOUR}"
 
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo nala update
 sudo nala install fish
 
-echo "Installing Fisher..."
+echo "${PLAINGREEN}Installing Fisher...${ENDCOLOUR}"
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher update
 
-echo "Installing Nala..."
+echo -e "${PLAINGREEN}Installing Nala...${ENDCOLOUR}"
 
 curl -sS https://starship.rs/install.sh | sh
 
-echo "World you like to install Junction? (y/n)"
+echo -e "${BOLDCYAN}World you like to install Junction? (y/n)${ENDCOLOUR}"
 
 read junction
 
 if [ $junction = "y" ]; then
+    echo -e "${PLAINGREEN}Installing Junction...${ENDCOLOUR}"
     flatpak install flathub re.sonny.Junction
     xdg-settings set default-web-browser re.sonny.Junction.desktop
 fi
 
-echo "Would you like to install NVM/NodeJS? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install NVM/NodeJS? (y/n)${ENDCOLOUR}"
 
 read nodejs
 
 if [ $nodejs = "y" ]; then
+    echo -e "${PLAINGREEN}Installing NVM/NodeJS...${ENDCOLOUR}"
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     source ~/.bashrc
     nvm install node
     nvm use node
 fi
 
-echo "Would you like to install Docker? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Docker? (y/n)${ENDCOLOUR}"
 
 read docker
 
 if [ $docker = "y" ]; then
+    echo -e "${PLAINGREEN}Installing Docker...${ENDCOLOUR}"
     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo nala remove $pkg; done
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -89,64 +106,74 @@ if [ $docker = "y" ]; then
     sudo nala install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
-echo "Would you like to install PHP 8.2? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install PHP 8.2? (y/n)${ENDCOLOUR}"
 
 read php
 
 if [ $php = "y" ]; then
+    echo -e "${PLAINGREEN}Installing PHP 8.2...${ENDCOLOUR}"
     sudo add-apt-repository ppa:ondrej/php
     sudo nala update
     sudo nala install php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-gd php8.2-mbstring php8.2-mysql php8.2-xml php8.2-zip php8.2-bcmath php8.2-intl php8.2-imagick php8.2-redis php8.2-xdebug
 fi
 
-echo "Would you like to install Composer? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Composer? (y/n)${ENDCOLOUR}"
 
 read composer
 
 if [ $composer = "y" ]; then
+    echo -e "${PLAINGREEN}Installing Composer...${ENDCOLOUR}"
     curl -sS https://getcomposer.org/installer | php
     sudo mv composer.phar ~/.local/bin/composer
 fi
 
-echo "Would you like to install Python 3? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Python 3 and 2? (y/n)${ENDCOLOUR}"
 
 read python
 
 if [ $python = "y" ]; then
-    sudo nala install python3 python3-pip python3-venv python3-dev
+    echo -e "${PLAINGREEN}Installing Python 3...${ENDCOLOUR}"
+    sudo nala install -y python3 python3-pip python3-venv python3-dev
+
+    echo -e "${PLAINGREEN}Installing Python 2...${ENDCOLOUR}"
+    sudo nala install -y python2 python-pip python-virtualenv python-dev
 fi
 
-echo "Would you like to install Ruby? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Ruby? (y/n)${ENDCOLOUR}"
 
 read ruby
 
 if [ $ruby = "y" ]; then
-    sudo nala install ruby-full
+    echo -e "${PLAINGREEN}Installing Ruby...${ENDCOLOUR}"
+    sudo nala install -y ruby-full
 fi
 
-echo "Would you like to install Rust? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Rust? (y/n)${ENDCOLOUR}"
 
 read rust
 
 if [ $rust = "y" ]; then
+    echo -e "${PLAINGREEN}Installing Rust...${ENDCOLOUR}"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 fi
 
-echo "Would you like to install Go? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Go? (y/n)${ENDCOLOUR}"
 
 read go
 
 if [ $go = "y" ]; then
-    sudo nala install golang-go
+    echo -e "${PLAINGREEN}Installing Go...${ENDCOLOUR}"
+    sudo nala install -y golang-go
 fi
 
-echo "Would you like to install Java 18? (y/n)"
+echo -e "${BOLDCYAN}Would you like to install Java 18? (y/n)${ENDCOLOUR}"
 
 read java
 
 if [ $java = "y" ]; then
-    sudo nala install openjdk-18-jdk
+    echo -e "${PLAINGREEN}Installing Java 18...${ENDCOLOUR}"
+    sudo nala install -y openjdk-18-jdk
 fi
 
-echo "Package installation done."
+echo -e "${BOLDGREEN}Package installation done!${ENDCOLOUR}"
